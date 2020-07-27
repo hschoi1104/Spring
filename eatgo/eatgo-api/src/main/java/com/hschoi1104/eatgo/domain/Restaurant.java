43c49254ca31609634e16a83dbeb396a93f3162c
hschoi1104 <hschoi1104@gmail.com>
2020-07-27 13:04:10 +0900

    Test Driven Development

---
+++ eatgo/eatgo-api/src/main/java/com/hschoi1104/eatgo/domain/Restaurant.java
@@ -0,0 +1,23 @@
+package com.hschoi1104.eatgo.domain;
+
+public class Restaurant {
+    private final String name;
+    private final String address;
+
+    public Restaurant(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getInformation() {
+        return name+" in "+address;
+    }
+}
---
+++ eatgo/eatgo-api/src/test/java/com/hschoi1104/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,24 @@
+package com.hschoi1104.eatgo.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class RestaurantTests {
+
+    @Test
+    public void creation(){
+        Restaurant restaurant = new Restaurant("Bob zip","Seoul");
+
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
+
+    @Test
+    public void information(){
+        Restaurant restaurant = new Restaurant("Bob zip","Seoul");
+
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+}
