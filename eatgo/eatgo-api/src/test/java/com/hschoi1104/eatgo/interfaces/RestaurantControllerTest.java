f77200efce9324b2489db7d49fc4b2f9f07dbc0b
hschoi1104 <hschoi1104@gmail.com>
2020-07-27 14:25:03 +0900

    Create GET /restaurants/{id}

--- eatgo/eatgo-api/src/test/java/com/hschoi1104/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo/eatgo-api/src/test/java/com/hschoi1104/eatgo/interfaces/RestaurantControllerTest.java
@@ -29,4 +29,16 @@ public class RestaurantControllerTest {
                 .andExpect(content().string(containsString("\"id\":1004")))
                 .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
     }
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\":1004")))
+                .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
+
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\":2020")))
+                .andExpect(content().string(containsString("\"name\":\"cyber food\"")));
+    }
 }
